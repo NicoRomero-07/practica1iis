b5ed79015d02e1c7dac453a1dec1698c575c9900
NicoRomero-07 <nicoromero07@uma.es>
2021-03-09 10:33:23 +0100

    cloneEquals

--- Jarra.java
+++ Jarra.java
@@ -12,5 +12,22 @@ this.capacidad=capacidadInicial;
 public void metodoComunitario(){
 // Comentario incluido para pruebas
 System.out.println("Aqui escribimos todos");
+
 }
+
+public Object clone() throws CloneNotSupportedException {
+        return (Jarra) super.clone();
+}
+
+
+public bool equals(Jarra otraJarra){
+    bool eqs = false;
+    if (null == otraJarra){ eqs = false; }
+    else if (this == otraJarra){ eqs = true; }
+    else if (otraJarra instanceOf Jarra.class){
+        eqs = this.capacidad == otraJarra.capacidad &&
+                    this.cantidad == otraJarra.cantidad;
+    } else { eqs = false; }
+    return eqs;
+    }
 }
